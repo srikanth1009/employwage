2cd4ee19f8ac0484b93ed058dbf669add85a15ec
srikanth1009 <srikanthreddyyaddala8.com>
2021-09-01 16:23:36 +0530

    salary caliculation

--- empsalary.sh
+++ empsalary.sh
@@ -3,16 +3,16 @@ isparttime=1;
 isfulltime=2;
 emprateperhr=20;
 empcheck=$((RANDOM%3));
-case $empcheck in 
-           $isfulltime)
-                 emphrs=8
+case $empcheck in
+$isfulltime)
+emphrs=8
 ;;
-            $isparttime)
-                 emphrs=4
+$isparttime)
+emphrs=4
 ;;
 *)
 emphrs=0
 ;;
 esac
 salary=$(($emphrs*$emprateperhr));
-empsalarycase.sh
+empsalary.sh (END)
